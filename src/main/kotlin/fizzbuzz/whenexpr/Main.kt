5c4f76628dfb2ab47b5069a59a35ec56cebe6dab
katzkb <katsuya7kubo@gmail.com>
2025-07-26 15:59:47 +0900

    feat: add FizzBuzz implementation using when expressions

---
+++ src/main/kotlin/fizzbuzz/whenexpr/Main.kt
@@ -0,0 +1,17 @@
+package fizzbuzz.whenexpr
+
+fun fizzbuzz(i: Int): String {
+  return when {
+    i % 15 == 0 -> "FizzBuzz"
+    i % 3 == 0 -> "Buzz"
+    i % 5 == 0 -> "Fizz"
+    else -> "$i"
+  }
+}
+
+fun main(args: Array<String>) {
+  val n = 20
+  for (i in 1..n) {
+    println(fizzbuzz(i))
+  }
+}
