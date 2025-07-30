5d81006fc18b71010737bf764b54ba60a8b1c92a
katzkb <katsuya7kubo@gmail.com>
2025-07-31 03:31:48 +0900

    chore: update Java version and dependencies in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,28 @@
 plugins {
-  kotlin("jvm") version "2.2.0"
-  kotlin("plugin.spring") version "1.9.25"
   id("org.springframework.boot") version "3.5.4"
   id("io.spring.dependency-management") version "1.1.7"
+
+  kotlin("jvm") version "2.0.21"
+  kotlin("plugin.spring") version "1.9.25"
+
+  /**
+   * detekt
+   *
+   * URL
+   * - https://github.com/detekt/detekt
+   * GradlePlugins(plugins.gradle.org)
+   * - https://plugins.gradle.org/plugin/io.gitlab.arturbosch.detekt
+   * Main用途
+   * - Linter/Formatter
+   * Sub用途
+   * - 無し
+   * 概要
+   * KotlinのLinter/Formatter
+   */
+  id("io.gitlab.arturbosch.detekt") version "1.23.8"
+
+  id("org.jetbrains.dokka") version "1.9.10"
+  id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
 }
 
 group = "org.example"
@@ -10,7 +30,7 @@ version = "1.0-SNAPSHOT"
 
 java {
   toolchain {
-    languageVersion.set(JavaLanguageVersion.of(24))
+    languageVersion.set(JavaLanguageVersion.of(21))
     vendor.set(JvmVendorSpec.AMAZON)
   }
 }
@@ -20,12 +40,24 @@ repositories {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-jdbc")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
-  runtimeOnly("org.postgresql:postgresql")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+  /**
+   * detektの拡張: detekt-formatting
+   *
+   * 概要
+   * - formattingのルール
+   * - 基本はktlintと同じ
+   * - format自動適用オプションの autoCorrect が使えるようになる
+   */
+  detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.8")
+
+  implementation("org.springframework.boot:spring-boot-starter-jdbc")
+  implementation("org.postgresql:postgresql")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
@@ -33,7 +65,6 @@ dependencies {
 kotlin {
   compilerOptions {
     freeCompilerArgs.addAll("-Xjsr305=strict")
-    freeCompilerArgs.add("-Xannotation-default-target=param-property")
   }
 }
 
