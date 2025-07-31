9e76e4257e97a2611f78fa3dc068552128913224
katzkb <katsuya7kubo@gmail.com>
2025-07-31 23:29:33 +0900

    feat: add ValidationError interface for domain object validation

---
+++ src/main/kotlin/com/example/server/implementing/util/ValidationError.kt
@@ -0,0 +1,12 @@
+package com.example.server.implementing.util
+
+/**
+ * The type of Error for validation at a domain object
+ */
+interface ValidationError {
+
+    /**
+     * Error message
+     */
+    val message: String
+}
